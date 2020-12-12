c43153ef94fc8a1b74de770135d03247feaae159
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-12-13 00:12:27 +0100

    Remove iconv prefix, since this is part of glibc in centos.

--- .github/workflows/pre-build-script-manylinux.sh
+++ .github/workflows/pre-build-script-manylinux.sh
@@ -9,7 +9,7 @@ BISON=bison-3.4
 curl -o bison.tar.gz http://ftp.gnu.org/gnu/bison/$BISON.tar.gz
 tar -xvzf bison.tar.gz
 cd $BISON
-./configure --prefix=/usr/local/ --with-libiconv-prefix=/usr/local/libiconv/
+./configure --prefix=/usr/local/
 make
 make install
 bison --version
