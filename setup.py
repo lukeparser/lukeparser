b358e4f8d2b28121014ea6641c6e69e1e5102a61
Sebastian Brodehl <foss@s14l.de>
2021-11-11 20:18:34 +0100

    Drop python 3.6

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ CLASSIFIERS = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
@@ -126,5 +125,5 @@ if __name__ == "__main__":
         },
         include_package_data=True,
         ext_modules=[extension_module],
-        python_requires='>=3.6',
+        python_requires='>=3.7',
     )
